2fe519bd78f4b21180820d602adc324ea01b74f1
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-09-20 12:15:51 -0700

    add scaffold html css js files for about me project and delete old class demo html file

---
+++ app.js
@@ -0,0 +1,15 @@
+var userName = prompt('What would your super hero name be?');
+console.log('The user\'s super hero name is ' + userName);
+alert('Hello ' + userName + '. ');
+
+var superPower = prompt('If you could have any super power what would it be?');
+console.log('The user\'s super power would be ' + superPower);
+alert('Hello ' + userName + '. '+ 'I hope you would use your super power of ' + superPower + ' for good.');
+
+var userCity = prompt('What mythical city would you live in?');
+console.log('The user would save the city of ' + userCity);
+alert('Hello ' + userName + '. '+ 'I hope you would use your super power of ' + superPower + ' to save ' + userCity);
+
+var nemesis = prompt('Who would be your arch nemesis?');
+console.log('The user\'s nemesis would be ' + nemesis);
+alert('Hello ' + userName + '. '+ 'I hope you would use your super power of ' + superPower + ' to save ' + userCity + ' and to defeat ' + nemesis + '!');
