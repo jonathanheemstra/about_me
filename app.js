651e050b45f174bcf5861d655aabc494bce529c1
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-09-22 16:53:49 -0700

    move questions insdie of functions

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ console.log('User\'s name: ' + userName);
 alert('Hi, ' + userName + '!\nAre you ready to play a fun guessing game?\nLet\'s get started!');
 
 //question 1 about my hair color
-var hairColor = prompt('Do I have blonde hair?\nPlease answer "yes" or "no"').toLowerCase();
-console.log('Question: Do I have blonde hair?');
 function question_1() {
+  var hairColor = prompt('Do I have blonde hair?\nPlease answer "yes" or "no"').toLowerCase();
+  console.log('Question: Do I have blonde hair?');
   if (hairColor === 'yes' || hairColor === 'y') {
     alert('Correct! I have had blonde hair for my whole life. Most of my family is blonde haired as well.');
     console.log('User\'s answer: ' + hairColor);
@@ -24,12 +24,11 @@ function question_1() {
     console.log('User answer did not contain a "yes" or "no"');
   }
 }
-question_1();
 
 //question 2 about my hometown
-var hometown = prompt('Is my hometown Bellingham, WA?\nPlease answer "yes" or "no"').toLowerCase();
-console.log('Question: Is my hometown Bellingham, WA?');
-function question_2 () {
+function question_2() {
+  var hometown = prompt('Is my hometown Bellingham, WA?\nPlease answer "yes" or "no"').toLowerCase();
+  console.log('Question: Is my hometown Bellingham, WA?');
   if (hometown === 'no' || hometown === 'n') {
     alert('Correct!\nMy hometown is actually Olympia WA. I moved to Bellingham after graduating from college.');
     console.log('User\'s answer: ' + hometown);
@@ -42,12 +41,11 @@ function question_2 () {
     console.log('User answer did not contain a "yes" or "no"');
   }
 }
-question_2 ();
 
 //question 3 about my previous job
-var previousJob = prompt('My previous job was in construction?\nPlease answer "yes" or "no"').toLowerCase();
-console.log('Question: My previous job was in construction?');
-function question_3 () {
+function question_3() {
+  var previousJob = prompt('My previous job was in construction?\nPlease answer "yes" or "no"').toLowerCase();
+  console.log('Question: My previous job was in construction?');
   if (previousJob === 'no' || previousJob === 'n') {
     alert('Correct!\nMy previous job was in marketing for a software company called Faithlife.');
     console.log('User\'s answer: ' + previousJob);
@@ -60,12 +58,11 @@ function question_3 () {
     console.log('User answer did not contain a "yes" or "no"');
   }
 }
-question_3 ();
 
 //question 4 about my favorite baseball team
-var favBaseballTeam = prompt('Is my favorite baseball team the Seattle Mariners?\nPlease answer "yes" or "no"').toLowerCase();
-console.log('Question: Is my favorite baseball team the Seattle Mariners?');
-function question_4 () {
+function question_4() {
+  var favBaseballTeam = prompt('Is my favorite baseball team the Seattle Mariners?\nPlease answer "yes" or "no"').toLowerCase();
+  console.log('Question: Is my favorite baseball team the Seattle Mariners?');
   if (favBaseballTeam === 'yes' || favBaseballTeam === 'y') {
     alert('Correct! I have been a huge Seattle Mariners fan since I was a little kid playing t-ball.');
     console.log('User\'s answer: ' + favBaseballTeam);
@@ -78,12 +75,11 @@ function question_4 () {
     console.log('User answer did not contain a "yes" or "no"');
   }
 }
-question_4 ();
 
 //question 5 about my nickname
-var myNickName = prompt('Is my nickname Jonny?\nPlease answer "yes" or "no"').toLowerCase();
-console.log('Question: Is my nickname Jonny?');
-function question_5 () {
+function question_5() {
+  var myNickName = prompt('Is my nickname Jonny?\nPlease answer "yes" or "no"').toLowerCase();
+  console.log('Question: Is my nickname Jonny?');
   if (myNickName === 'yes' || myNickName === 'y') {
     alert('Correct! I have gone by the nickname "Jonny" since 2nd grade.');
     console.log('User\'s answer: ' + myNickName);
@@ -96,12 +92,11 @@ function question_5 () {
     console.log('User answer did not contain a "yes" or "no"');
   }
 }
-question_5 ();
 
 //question 6 guess how many years I played baseball
-var randomNumber = Math.floor(Math.random() * 20) + 1; //had done work with random numbers previously but had to consult http://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript to jog my memory
-console.log('Random number generated for question 6: ' + randomNumber);
-function question_6 () {
+function question_6() {
+  var randomNumber = Math.floor(Math.random() * 20) + 1; //had done work with random numbers previously but had to consult http://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript to jog my memory
+  console.log('Random number generated for question 6: ' + randomNumber);
   for (var i = 0; i < 4; i++) {
     var yearsPlaying = parseFloat(prompt('How many years did I play baseball?\nPlease provide a number between 0 and 20.'));
     var remainingGuesses = 4 - i;
@@ -123,12 +118,11 @@ function question_6 () {
     alert('Sorry, you didn\'t guess the right number. The right number was ' + randomNumber);
   }
 }
-question_6();
 
 //question 7 what are my favorite things
-var myFavoriteThings = ['backcountry skiing', 'hanging out with friends', 'hiking', 'rock climbing', 'running', 'playing board games', 'watching classic sitcoms', 'kayaking', 'playing video games', 'watching baseball'];
-var a = 0;
 function question_7() {
+  var myFavoriteThings = ['backcountry skiing', 'hanging out with friends', 'hiking', 'rock climbing', 'running', 'playing board games', 'watching classic sitcoms', 'kayaking', 'playing video games', 'watching baseball'];
+  var a = 0;
   while (a < 6) {
     var favoriteThings = prompt('Can you guess one of my favorite things to do in my spare time?');
     favoriteThings = favoriteThings.toLowerCase();
@@ -155,11 +149,10 @@ function question_7() {
     alert('Sorry, you didn\'t guess any of my favorite things to do. Some of my favorite things to do are ' + myFavoriteThings);
   }
 }
-question_7 ();
 
 //Results
-console.log('User\'s final score: ' + userScore + ' out of 7');
-function results (){
+function results(){
+  console.log('User\'s final score: ' + userScore + ' out of 7');
   if (userScore >= 6) {
     alert(userName + ', Your final score was ' + userScore + ' out of 7.\nGood work! You really know me well or are really good at guessing.' );
   } else if (userScore >= 4) {
@@ -167,7 +160,14 @@ function results (){
   } else {
     alert(userName + ', Your final score was ' + userScore + ' out of 7.\nI think we should hangout sometime because you don\'t know very much about me');
   }
+  document.write('<h4 class="final_score">Your Score:<br>' + userScore + ' out of 7</h4>');
 }
-results ();
 
-document.write('<h4 class="final_score">Your Score:<br>' + userScore + ' out of 7</h4>');
+question_1();
+question_2();
+question_3();
+question_4();
+question_5();
+question_6();
+question_7();
+results();
